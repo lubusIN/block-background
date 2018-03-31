13865fe4a16e58d78faf34e57b47709ffc6ec35c
Ajit Bohra <ajit@lubus.in>
2018-04-01 03:10:46 +0530

    Add attributes

---
+++ src/data/attributes.js
@@ -0,0 +1,25 @@
+/**
+ * Attributes Object
+ */
+const backgroundSettings = {
+	backgroundType: {
+		type: 'string',
+	},
+	solidColor: {
+		type: 'string',
+	},
+	gradient: {
+		type: 'string',
+	},
+	image: {
+		type: 'string',
+	},
+	overlay: {
+		type: 'string',
+	},
+	opacity: {
+		type: 'string',
+	},
+};
+
+export default backgroundSettings;
