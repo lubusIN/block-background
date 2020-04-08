20ccc4eb513e0cd01421100c259fcecb1e872c27
Naveen Kharwar <naveen919827@gmail.com>
2020-04-08 15:18:51 +0530

    - group button style fixed

--- src/components/inspector.js
+++ src/components/inspector.js
@@ -4,18 +4,18 @@
 import { __ } from "@wordpress/i18n";
 
 import {
-  InspectorControls,
-  MediaUpload,
-  ColorPalette
+	InspectorControls,
+	MediaUpload,
+	ColorPalette
 } from "@wordpress/editor";
 
 import {
-  PanelBody,
-  Button,
-  ButtonGroup,
-  SelectControl,
-  ToggleControl,
-  RangeControl
+	PanelBody,
+	Button,
+	ButtonGroup,
+	SelectControl,
+	ToggleControl,
+	RangeControl
 } from "@wordpress/components";
 
 import { Fragment } from "@wordpress/element";
@@ -32,188 +32,189 @@ import gradients from "../data/gradients";
  * @returns {object} Component
  */
 const Inspector = props => {
-  // Attributes
-  const { attributes, setAttributes } = props;
-  const {
-    backgroundType,
-    solidColor,
-    gradient,
-    mediaID,
-    mediaURL,
-    overlay,
-    overlayType,
-    opacity
-  } = attributes;
+	// Attributes
+	const { attributes, setAttributes } = props;
+	const {
+		backgroundType,
+		solidColor,
+		gradient,
+		mediaID,
+		mediaURL,
+		overlay,
+		overlayType,
+		opacity
+	} = attributes;
 
-  // Event(s)
-  const onChangeBackgroundType = newType => {
-    setAttributes({
-      backgroundType: newType,
-      overlay: undefined,
-      overlayType: undefined
-    });
-  };
-  const onChangeSolidColor = newColor => {
-    setAttributes({ solidColor: newColor });
-  };
-  const onChangeGradient = newGradient => {
-    setAttributes({ gradient: newGradient });
-  };
-  const onSelectImage = media => {
-    setAttributes({
-      mediaID: media.id,
-      mediaURL: media.url
-    });
-  };
-  const onRemoveImage = () => {
-    setAttributes({
-      mediaID: undefined,
-      mediaURL: undefined
-    });
-  };
-  const onChangeOverlay = newOverlay => {
-    setAttributes({ overlay: newOverlay });
-  };
-  const onChangeOverlayType = newType => {
-    setAttributes({ overlayType: newType });
-  };
-  const onChangeOpacity = newOpacity => {
-    setAttributes({ opacity: newOpacity });
-  };
+	// Event(s)
+	const onChangeBackgroundType = newType => {
+		setAttributes({
+			backgroundType: newType,
+			overlay: undefined,
+			overlayType: undefined
+		});
+	};
+	const onChangeSolidColor = newColor => {
+		setAttributes({ solidColor: newColor });
+	};
+	const onChangeGradient = newGradient => {
+		setAttributes({ gradient: newGradient });
+	};
+	const onSelectImage = media => {
+		setAttributes({
+			mediaID: media.id,
+			mediaURL: media.url
+		});
+	};
+	const onRemoveImage = () => {
+		setAttributes({
+			mediaID: undefined,
+			mediaURL: undefined
+		});
+	};
+	const onChangeOverlay = newOverlay => {
+		setAttributes({ overlay: newOverlay });
+	};
+	const onChangeOverlayType = newType => {
+		setAttributes({ overlayType: newType });
+	};
+	const onChangeOpacity = newOpacity => {
+		setAttributes({ opacity: newOpacity });
+	};
 
-  // Controls
-  const colorControl = (
-    <ColorPalette value={solidColor} onChange={onChangeSolidColor} />
-  );
+	// Controls
+	const colorControl = (
+		<ColorPalette value={solidColor} onChange={onChangeSolidColor} />
+	);
 
-  const gradientControl = (
-    <SelectControl
-      label={__("Gradient Presets")}
-      options={gradients.map(preset => {
-        return {
-          value: preset.colors,
-          label: preset.name
-        };
-      })}
-      value={gradient}
-      onChange={onChangeGradient}
-    />
-  );
+	const gradientControl = (
+		<SelectControl
+			label={__("Gradient Presets")}
+			options={gradients.map(preset => {
+				return {
+					value: preset.colors,
+					label: preset.name
+				};
+			})}
+			value={gradient}
+			onChange={onChangeGradient}
+		/>
+	);
 
-  // Inspector Controls
-  return (
-    <InspectorControls>
-      <PanelBody title={__("Background Settings")} initialOpen={true}>
-        <ButtonGroup
-          aria-label={__("Background Type")}
-          className="block-background__type"
-        >
-          {options.backgroundType.map(type => {
-            return (
-              <Button
-                key={type.label}
-                isLarge
-                isPrimary={backgroundType === type.value}
-                aria-pressed={backgroundType === type.value}
-                onClick={() => onChangeBackgroundType(type.value)}
-              >
-                {type.label}
-              </Button>
-            );
-          })}
-        </ButtonGroup>
+	// Inspector Controls
+	return (
+		<InspectorControls>
+			<PanelBody title={__("Background Settings")} initialOpen={true}>
+				<ButtonGroup
+					aria-label={__("Background Type")}
+					className="block-background__type"
+				>
+					{options.backgroundType.map(type => {
+						return (
+							<Button
+								key={type.label}
+								isSmall
+								isSecondary
+								isPrimary={backgroundType === type.value}
+								aria-pressed={backgroundType === type.value}
+								onClick={() => onChangeBackgroundType(type.value)}
+							>
+								{type.label}
+							</Button>
+						);
+					})}
+				</ButtonGroup>
 
-        {"color" === backgroundType && colorControl}
+				{"color" === backgroundType && colorControl}
 
-        {"gradient" === backgroundType && gradientControl}
+				{"gradient" === backgroundType && gradientControl}
 
-        {"image" === backgroundType && (
-          <MediaUpload
-            key="mediaupload"
-            onSelect={onSelectImage}
-            type="image"
-            value={mediaID}
-            render={({ open }) => (
-              <Fragment>
-                <Button className="button-link" onClick={open}>
-                  {!mediaID ? (
-                    __("Set background image")
-                  ) : (
-                    <img src={mediaURL} />
-                  )}
-                </Button>
-              </Fragment>
-            )}
-          />
-        )}
+				{"image" === backgroundType && (
+					<MediaUpload
+						key="mediaupload"
+						onSelect={onSelectImage}
+						type="image"
+						value={mediaID}
+						render={({ open }) => (
+							<Fragment>
+								<Button className="button-link" onClick={open}>
+									{!mediaID ? (
+										__("Set background image")
+									) : (
+										<img src={mediaURL} />
+									)}
+								</Button>
+							</Fragment>
+						)}
+					/>
+				)}
 
-        {// Actions for background image selected
-        "image" === backgroundType &&
-          mediaID && (
-            <Fragment>
-              <p className="editor-post-featured-image__howto">
-                {__("Click the image to edit or update")}
-              </p>
-              <Button
-                className="button-link"
-                style={{ marginBottom: "20px" }}
-                onClick={onRemoveImage}
-              >
-                {__("Remove background image")}
-              </Button>
-            </Fragment>
-          )}
+				{// Actions for background image selected
+				"image" === backgroundType &&
+					mediaID && (
+						<Fragment>
+							<p className="editor-post-featured-image__howto">
+								{__("Click the image to edit or update")}
+							</p>
+							<Button
+								className="button-link"
+								style={{ marginBottom: "20px" }}
+								onClick={onRemoveImage}
+							>
+								{__("Remove background image")}
+							</Button>
+						</Fragment>
+					)}
 
-        {"image" === backgroundType &&
-          mediaID && (
-            <ToggleControl
-              key="togglecontrol"
-              label={__("Overlay")}
-              checked={!!overlay}
-              onChange={onChangeOverlay}
-            />
-          )}
+				{"image" === backgroundType &&
+					mediaID && (
+						<ToggleControl
+							key="togglecontrol"
+							label={__("Overlay")}
+							checked={!!overlay}
+							onChange={onChangeOverlay}
+						/>
+					)}
 
-        {overlay && (
-          <ButtonGroup aria-label={__("Overlay Type")} key="overlaytype">
-            {options.backgroundType.map(type => {
-              if ("image" === type.value) {
-                return;
-              }
+				{overlay && (
+					<ButtonGroup aria-label={__("Overlay Type")} key="overlaytype">
+						{options.backgroundType.map(type => {
+							if ("image" === type.value) {
+								return;
+							}
 
-              return (
-                <Button
-                  key={type.label}
-                  isLarge
-                  isPrimary={overlayType === type.value}
-                  aria-pressed={overlayType === type.value}
-                  onClick={() => onChangeOverlayType(type.value)}
-                >
-                  {type.label}
-                </Button>
-              );
-            })}
-          </ButtonGroup>
-        )}
+							return (
+								<Button
+									key={type.label}
+									isLarge
+									isPrimary={overlayType === type.value}
+									aria-pressed={overlayType === type.value}
+									onClick={() => onChangeOverlayType(type.value)}
+								>
+									{type.label}
+								</Button>
+							);
+						})}
+					</ButtonGroup>
+				)}
 
-        {overlay && "color" === overlayType && colorControl}
+				{overlay && "color" === overlayType && colorControl}
 
-        {overlay && "gradient" === overlayType && gradientControl}
+				{overlay && "gradient" === overlayType && gradientControl}
 
-        {overlay &&
-          ("color" === overlayType || "gradient" === overlayType) && (
-            <RangeControl
-              key="rangecontrol"
-              label="Opacity"
-              value={opacity}
-              onChange={onChangeOpacity}
-              min={1}
-              max={10}
-            />
-          )}
-      </PanelBody>
-    </InspectorControls>
-  );
+				{overlay &&
+					("color" === overlayType || "gradient" === overlayType) && (
+						<RangeControl
+							key="rangecontrol"
+							label="Opacity"
+							value={opacity}
+							onChange={onChangeOpacity}
+							min={1}
+							max={10}
+						/>
+					)}
+			</PanelBody>
+		</InspectorControls>
+	);
 };
 
 export default Inspector;
